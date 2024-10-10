7093c0cd7bf0db6b61a93772fe1cf4981596ec1a
tauane <tauane-soares@hotmail.com>
2024-10-10 09:16:43 -0300

    camada de segurança

--- conexao.js
+++ conexao.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-const endereco = "mongodb+srv://tauanesoares:PcRSdYiZyF9yrhT@cluster0.p2cvb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+const endereco = process.env.MONGO_URI
 const configuracao = {useNewUrlParser: true, useUnifiedTopology: true}
 
 await mongoose.connect(endereco, configuracao)
